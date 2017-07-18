0c8eac85491b18d47717e032ca0a3410c508c96b
Gao Chaoyu <bikegcy@gmail.com>
2017-07-18 17:56:25 +0800

    Update e01_sol.py
    show both the std solution and my naive solution

--- assignments/exercises/e01_sol.py
+++ assignments/exercises/e01_sol.py
@@ -1,7 +1,8 @@
 """
-Solution to simple TensorFlow exercises
-For the problems 
+Simple TensorFlow exercises
+You should thoroughly test your code
 """
+
 import tensorflow as tf
 
 ###############################################################################
@@ -14,6 +15,8 @@ import tensorflow as tf
 x = tf.random_uniform([])  # Empty array as shape creates a scalar.
 y = tf.random_uniform([])
 out = tf.cond(tf.greater(x, y), lambda: tf.add(x, y), lambda: tf.subtract(x, y))
+print('x :', x, ' y:', y)
+print('out:', out)
 
 ###############################################################################
 # 1b: Create two 0-d tensors x and y randomly selected from the range [-1, 1).
@@ -21,28 +24,34 @@ out = tf.cond(tf.greater(x, y), lambda: tf.add(x, y), lambda: tf.subtract(x, y))
 # Hint: Look up tf.case().
 ###############################################################################
 
-x = tf.random_uniform([], -1, 1, dtype=tf.float32)
-y = tf.random_uniform([], -1, 1, dtype=tf.float32)
-out = tf.case({tf.less(x, y): lambda: tf.add(x, y), 
-			tf.greater(x, y): lambda: tf.subtract(x, y)}, 
+x = tf.random_uniform([], minval=-1, maxval=1)
+y = tf.random_uniform([], minval=-1, maxval=1)
+# out = tf.case(tf.less(x,y): tf.add(x, y), tf.greater(x, y): tf.subtract(x, y))
+out = tf.case({tf.less(x, y): lambda: tf.add(x, y),
+			tf.greater(x, y): lambda: tf.subtract(x, y)},
 			default=lambda: tf.constant(0.0), exclusive=True)
-print(x)
-sess = tf.InteractiveSession()
-print(sess.run(x))
+print('x :', x, ' y:', y)
+print('out:', out)
 
 ###############################################################################
-# 1c: Create the tensor x of the value [[0, -2, -1], [0, 1, 2]] 
+# 1c: Create the tensor x of the value [[0, -2, -1], [0, 1, 2]]
 # and y as a tensor of zeros with the same shape as x.
 # Return a boolean tensor that yields Trues if x equals y element-wise.
 # Hint: Look up tf.equal().
 ###############################################################################
 
-x = tf.constant([[0, -2, -1], [0, 1, 2]])
-y = tf.zeros_like(x)
-out = tf.equal(x, y)
+#x = tf.constant([[0, -2, -1], [0, 1, 2]], shape = (2,3))
+#y = tf.zeros_like(x)
+#out = tf.equal(x, y)
+x = tf.constant([[0, -2, -1], [0, 1, 2]], shape = (2,3))
+y = tf.zeros((tf.shape(x)))
+out = tf.equal(tf.shape(x), tf.shape(y))
+print('x :', x, ' y:', y)
+print('out:', out)
+
 
 ###############################################################################
-# 1d: Create the tensor x of value 
+# 1d: Create the tensor x of value
 # [29.05088806,  27.61298943,  31.19073486,  29.35532951,
 #  30.97266006,  26.67541885,  38.08450317,  20.74983215,
 #  34.94445419,  34.45999146,  29.06485367,  36.01657104,
@@ -54,22 +63,27 @@ out = tf.equal(x, y)
 # Hint: Use tf.gather().
 ###############################################################################
 
-x = tf.constant([29.05088806,  27.61298943,  31.19073486,  29.35532951,
-		        30.97266006,  26.67541885,  38.08450317,  20.74983215,
-		        34.94445419,  34.45999146,  29.06485367,  36.01657104,
-		        27.88236427,  20.56035233,  30.20379066,  29.51215172,
-		        33.71149445,  28.59134293,  36.05556488,  28.66994858])
-indices = tf.where(x > 30)
-out = tf.gather(x, indices)
+x = tf.constant(
+    [29.05088806,  27.61298943,  31.19073486,  29.35532951,
+    30.97266006,  26.67541885,  38.08450317,  20.74983215,
+    34.94445419,  34.45999146,  29.06485367,  36.01657104,
+    27.88236427,  20.56035233,  30.20379066,  29.51215172,
+    33.71149445,  28.59134293,  36.05556488,  28.66994858])
+y = tf.where(tf.greater(x,[30]))
+out = tf.gather(x, y)
+#indices = tf.where(x > 30)
+#out = tf.gather(x, indices)
+print('x :', x, ' y:', y)
+print('out:', out)
 
 ###############################################################################
-# 1e: Create a diagnoal 2-d tensor of size 6 x 6 with the diagonal values of 1,
+# 1e: Create a diagonal 2-d tensor of size 6 x 6 with the diagonal values of 1,
 # 2, ..., 6
 # Hint: Use tf.range() and tf.diag().
 ###############################################################################
 
-values = tf.range(1, 7)
-out = tf.diag(values)
+out = tf.diag(tf.range(1, 1, 7))
+print('out: ', out)
 
 ###############################################################################
 # 1f: Create a random 2-d tensor of size 10 x 10 from any distribution.
@@ -77,8 +91,9 @@ out = tf.diag(values)
 # Hint: Look at tf.matrix_determinant().
 ###############################################################################
 
-m = tf.random_normal([10, 10], mean=10, stddev=1)
-out = tf.matrix_determinant(m)
+x = tf.random_uniform([10,10])
+out = tf.matrix_determinant(x)
+print('out: ', out)
 
 ###############################################################################
 # 1g: Create tensor x with value [5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9].
@@ -87,7 +102,9 @@ out = tf.matrix_determinant(m)
 ###############################################################################
 
 x = tf.constant([5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9])
-unique_values, indices = tf.unique(x)
+y, idx = tf.unique(x)
+
+print('out: ', y)
 
 ###############################################################################
 # 1h: Create two tensors x and y of shape 300 from any normal distribution,
@@ -99,9 +116,16 @@ unique_values, indices = tf.unique(x)
 # Hint: see the Huber loss function in the lecture slides 3.
 ###############################################################################
 
-x = tf.random_normal([300], mean=5, stddev=1)
-y = tf.random_normal([300], mean=5, stddev=1)
-average = tf.reduce_mean(x - y)
-def f1(): return tf.reduce_mean(tf.square(x - y))
-def f2(): return tf.reduce_sum(tf.abs(x - y))
-out = tf.cond(average < 0, f1, f2)
+x = tf.random_normal([300])
+y = tf.random_normal([300])
+sub = x - y
+condition = tf.less(tf.reduce_mean(sub), 0)
+neg_res = tf.reduce_mean(tf.square(sub))
+pos_res = tf.reduce_sum(tf.abs(sub))
+out = tf.cond(condition, lambda: neg_res, lambda: pos_res)
+print('out: ', out)
+
+
+
+
+
